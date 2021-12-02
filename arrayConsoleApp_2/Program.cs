47616e49174ddde40e833c8f7933434413da2a42
Murat Karasah <mkarasah@gmail.com>
2021-12-02 10:46:18 +0300

    Add project files.

---
+++ arrayConsoleApp_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace arrayConsoleApp_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isim = { "ali", "mehmet", "nuri"};
+            int[] s = new int[5];
+            int[] a = new int[4] { 1, 2, 3, 4 };
+
+
+        }
+    }
+}
