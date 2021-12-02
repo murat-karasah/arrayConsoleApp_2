1a60e7ddd17ca18b450e4e7db2074e2800c9b87d
Murat Karasah <mkarasah@gmail.com>
2021-12-02 13:01:03 +0300

    the end

--- arrayConsoleApp_2/Program.cs
+++ arrayConsoleApp_2/Program.cs
@@ -6,66 +6,72 @@ namespace arrayConsoleApp_2
     {
         static void Main(string[] args)
         {
-            /*
-            string[] isim = { "ali", "mehmet", "nuri"};
-            int[] s = new int[5];
-            int[] a = new int[4] { 1, 2, 3, 4 };
-            int[] b = new int[3];
-            b[0] = 5;
-            b[1] = 3;
-            b[2] = 4;
-            */
 
-            /*foreach (int item in b)
-            {
-                Console.WriteLine(item);
-            }*/
+            //string[] isim = { "ali", "mehmet", "nuri" };
+            //int[] s = new int[5];
+            //int[] a = new int[4] { 1, 2, 3, 4 };
+            //int[] b = new int[3];
+            //b[0] = 5;
+            //b[1] = 3;
+            //b[2] = 4;
 
-            /*
-            for (int i = 0; i <b.Length; i++)
-            {
-                Console.WriteLine(b[i]);
-            }
-            */
+
+            //foreach (int item in b)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            //for (int i = 0; i < b.Length; i++)
+            //{
+            //    Console.WriteLine(b[i]);
+            //}
+
             // Array console creat
-            /*
-            Console.WriteLine("int veri türünde bir dizi oluşturuluyor... Dizi kaç elemanlı olsun?");
-            int enterArray = Convert.ToInt32(Console.ReadLine());
-            int[] dizim = new int[enterArray];
-            Console.WriteLine("Şimdi Dizi Elemanları Girmeye Başlayalım");
-            int sayi=0;
-            for (int i = 0; i < dizim.Length; i++)
-            {
-                Console.WriteLine($"{i}.Dizi için int türünde bir veri giriniz: ");
-                dizim[i] = Convert.ToInt32(Console.ReadLine());
-                sayi += dizim[i];
-            }
-            double ortalama = Convert.ToDouble(sayi) / Convert.ToDouble(dizim.Length);
-            Console.Clear();
-            Console.WriteLine("Dizi Oluşturma Tamamlandı..");
-            Console.WriteLine($"Dizinin elemanlarının toplamı : {sayi}\nDizinin Elemanlarının ortalaması : {ortalama}");
-            foreach (int item in dizim)
-            {
-                Console.WriteLine(item);
-            }
-            */
 
-            Random rnd = new Random();
-            int[] dizim = new int[20];
-            int sayac = 0;
-            for (int i = 0; i < dizim.Length; i++)
-            {
-                dizim[i] = rnd.Next(1, 10);
-            }
-            foreach (int item in dizim)
-            {
-                Console.WriteLine(item);
-                if (item==4)
-                {
-                    sayac++;
-                }
-            }
-            Console.WriteLine($"toplam {sayac} adet 4 elemanı vardır.");
+            //Console.WriteLine("int veri türünde bir dizi oluşturuluyor... Dizi kaç elemanlı olsun?");
+            //int enterArray = Convert.ToInt32(Console.ReadLine());
+            //int[] dizim = new int[enterArray];
+            //Console.WriteLine("Şimdi Dizi Elemanları Girmeye Başlayalım");
+            //int sayi = 0;
+            //for (int i = 0; i < dizim.Length; i++)
+            //{
+            //    Console.WriteLine($"{i}.Dizi için int türünde bir veri giriniz: ");
+            //    dizim[i] = Convert.ToInt32(Console.ReadLine());
+            //    sayi += dizim[i];
+            //}
+            //double ortalama = Convert.ToDouble(sayi) / Convert.ToDouble(dizim.Length);
+            //Console.Clear();
+            //Console.WriteLine("Dizi Oluşturma Tamamlandı..");
+            //Console.WriteLine($"Dizinin elemanlarının toplamı : {sayi}\nDizinin Elemanlarının ortalaması : {ortalama}");
+            //foreach (int item in dizim)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+            // random eleman üretme...
+
+            //Random rnd = new Random();
+            //int[] dizim = new int[20];
+            //int sayac = 0;
+            //for (int i = 0; i < dizim.Length; i++)
+            //{
+            //    dizim[i] = rnd.Next(1, 10);
+            //}
+            //foreach (int item in dizim)
+            //{
+            //    Console.WriteLine(item);
+            //    if (item==4)
+            //    {
+            //        sayac++;
+            //    }
+            //}
+            //Console.WriteLine($"toplam {sayac} adet 4 elemanı vardır.");
+
+
+
         }
     }
 }
